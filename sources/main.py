d91a989a984cf86b6112c77218722be4e92e6048
CDFMLR <cdfmlr@murchinroom.fun>
2024-12-02 12:27:46 +0800

    fix(contributed_nodes): sync from anmol098/waka-readme-stats

--- sources/main.py
+++ sources/main.py
@@ -139,7 +139,7 @@ async def collect_user_repositories() -> Dict:
     DBM.g("\tUser repository list collected!")
 
     contributed = await DM.get_remote_graphql("repos_contributed_to", username=GHM.USER.login)
-    contributed_nodes = [r for r in contributed["data"]["user"]["repositoriesContributedTo"]["nodes"] if r and r["name"] not in repo_names and not r["isFork"]]
+    contributed_nodes = [repo for repo in contributed if repo is not None and repo["name"] not in repo_names and not repo["isFork"]]
     DBM.g("\tUser contributed to repository list collected!")
 
     return repositories + contributed_nodes
